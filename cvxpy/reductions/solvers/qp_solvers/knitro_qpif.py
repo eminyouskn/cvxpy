272c124b8ff3e7a888b6f526ca53f17c6ddc4d91
Youssouf Emine <youssouf.emine@artelys.com>
2025-05-13 10:36:58 -0400

    Add KNITRO QP solver interface implementation

---
+++ cvxpy/reductions/solvers/qp_solvers/knitro_qpif.py
@@ -0,0 +1,291 @@
+"""
+Copyright, the CVXPY authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import numpy as np
+
+import cvxpy.settings as s
+from cvxpy.reductions.solution import Solution, failure_solution
+from cvxpy.reductions.solvers.qp_solvers.qp_solver import QpSolver
+from cvxpy.utilities.citations import CITATION_DICT
+
+
+def kn_coo(*mats) -> tuple[list[int], list[int], list[float]]:
+    co_idxs = []
+    var_idxs = []
+    coefs = []
+    for mat in mats:
+        co_idxs.extend(mat.row)
+        var_idxs.extend(mat.col)
+        coefs.extend(mat.data)
+    return co_idxs, var_idxs, coefs
+
+
+class KNITRO(QpSolver):
+    """QP interface for the Knitro solver"""
+
+    MIP_CAPABLE = True
+    BOUNDED_VARIABLES = True
+
+    # Keyword arguments for the CVXPY interface.
+    INTERFACE_ARGS = []
+
+    # Map of Knitro status to CVXPY status.
+    STATUS_MAP = {
+        0: s.OPTIMAL,
+        -100: s.OPTIMAL_INACCURATE,
+        -101: s.USER_LIMIT,
+        -102: s.USER_LIMIT,
+        -103: s.USER_LIMIT,
+        -200: s.INFEASIBLE,
+        -201: s.INFEASIBLE,
+        -202: s.INFEASIBLE,
+        -203: s.INFEASIBLE,
+        -204: s.INFEASIBLE,
+        -205: s.INFEASIBLE,
+        -300: s.UNBOUNDED,
+        -301: s.INFEASIBLE_OR_UNBOUNDED,
+        -400: s.USER_LIMIT,
+        -401: s.USER_LIMIT,
+        -402: s.USER_LIMIT,
+        -403: s.USER_LIMIT,
+        -404: s.USER_LIMIT,
+        -405: s.USER_LIMIT,
+        -406: s.USER_LIMIT,
+        -410: s.USER_LIMIT,
+        -411: s.USER_LIMIT,
+        -412: s.USER_LIMIT,
+        -413: s.USER_LIMIT,
+        -415: s.USER_LIMIT,
+        -416: s.USER_LIMIT,
+        -500: s.SOLVER_ERROR,
+        -501: s.SOLVER_ERROR,
+        -502: s.SOLVER_ERROR,
+        -503: s.SOLVER_ERROR,
+        -504: s.SOLVER_ERROR,
+        -505: s.SOLVER_ERROR,
+        -506: s.SOLVER_ERROR,
+        -507: s.SOLVER_ERROR,
+        -508: s.SOLVER_ERROR,
+        -509: s.SOLVER_ERROR,
+        -510: s.SOLVER_ERROR,
+        -511: s.SOLVER_ERROR,
+        -512: s.SOLVER_ERROR,
+        -513: s.SOLVER_ERROR,
+        -514: s.SOLVER_ERROR,
+        -515: s.SOLVER_ERROR,
+        -516: s.SOLVER_ERROR,
+        -517: s.SOLVER_ERROR,
+        -518: s.SOLVER_ERROR,
+        -519: s.SOLVER_ERROR,
+        -520: s.SOLVER_ERROR,
+        -521: s.SOLVER_ERROR,
+        -522: s.SOLVER_ERROR,
+        -523: s.SOLVER_ERROR,
+        -524: s.SOLVER_ERROR,
+        -525: s.SOLVER_ERROR,
+        -526: s.SOLVER_ERROR,
+        -527: s.SOLVER_ERROR,
+        -528: s.SOLVER_ERROR,
+        -529: s.SOLVER_ERROR,
+        -530: s.SOLVER_ERROR,
+        -531: s.SOLVER_ERROR,
+        -532: s.SOLVER_ERROR,
+        -600: s.SOLVER_ERROR,
+    }  # MEM_LIMIT
+
+    # Object Key:
+    KEY = "context"
+
+    def name(self):
+        return s.KNITRO
+
+    def import_solver(self) -> None:
+        import knitro
+
+        knitro
+
+    def apply(self, problem):
+        """
+        Construct QP problem data stored in a dictionary.
+        The QP has the following form
+
+            minimize      1/2 x' P x + q' x
+            subject to    A x =  b
+                          F x <= g
+
+        """
+        return super(KNITRO, self).apply(problem)
+
+    def invert(self, results, inverse_data):
+        import knitro as kn
+
+        kc = results[KNITRO.KEY]
+        status_kn, obj_kn, x_kn, y_kn = kn.KN_get_solution(kc)
+        num_iters = kn.KN_get_number_iters(kc)
+        solve_time = kn.KN_get_solve_time_real(kc)
+        attr = {
+            s.SOLVE_TIME: solve_time,
+            s.NUM_ITERS: num_iters,
+            s.EXTRA_STATS: kc,
+        }
+        status = self.STATUS_MAP.get(status_kn, s.SOLVER_ERROR)
+        if (status not in s.SOLUTION_PRESENT) or (x_kn is None):
+            return failure_solution(status, attr)
+
+        obj = obj_kn + inverse_data[s.OFFSET]
+        x = np.array(x_kn)
+        primal_vars = {KNITRO.VAR_ID: x}
+
+        dual_vars = None
+        if y_kn is not None:
+            y = np.array(y_kn)
+            dual_vars = {KNITRO.DUAL_VAR_ID: y}
+
+        return Solution(status, obj, primal_vars, dual_vars, attr)
+
+    def solve_via_data(self, data, warm_start: bool, verbose: bool, solver_opts, solver_cache=None):
+        """
+        Returns the result of the call to the solver.
+
+        Parameters
+        ----------
+        data : dict
+            Data used by the solver.
+        warm_start : bool
+            Not used.
+        verbose : bool
+            Should the solver print output?
+        solver_opts : dict
+            Additional arguments for the solver.
+        solver_cache: None
+            None
+
+        Returns
+        -------
+        tuple
+            (status, optimal value, primal, equality dual, inequality dual)
+        """
+        import knitro as kn
+
+        P = data[s.P]
+        q = data[s.Q]
+        A = data[s.A].tocoo()
+        b = data[s.B]
+        F = data[s.F].tocoo()
+        g = data[s.G]
+        n_vars = int(data["n_var"])
+        lb = data[s.LOWER_BOUNDS]
+        ub = data[s.UPPER_BOUNDS]
+
+        if solver_opts:
+            # Solver options are not yet supported in this interface.
+            raise ValueError("Solver options are not supported in the Knitro interface.")
+
+        try:
+            kc = kn.KN_new()
+        except Exception:
+            raise ValueError("Valid Knitro license not found. Please check your installation.")
+
+        if not verbose:
+            # Disable Knitro output.
+            kn.KN_set_int_param(kc, kn.KN_PARAM_OUTLEV, kn.KN_OUTLEV_NONE)
+
+        # Add n variables to the problem.
+        kn.KN_add_vars(kc, n_vars)
+
+        # Set initial values for the variables.
+        if warm_start:
+            # Warm start is not supported in this interface.
+            raise ValueError("Warm start is not supported in the Knitro interface.")
+
+        # Set the lower and upper bounds on the variables.
+        if lb is not None:
+            kn.KN_set_var_lobnds(kc, xLoBnds=lb)
+        if ub is not None:
+            kn.KN_set_var_upbnds(kc, xUpBnds=ub)
+
+        # Set the variable types.
+        # - default: KN_VARTYPE_CONTINUOUS.
+        # - binray: KN_VARTYPE_BINARY.
+        # - integer: KN_VARTYPE_INTEGER.
+        var_types = [kn.KN_VARTYPE_CONTINUOUS] * n_vars
+        for ind in data[s.BOOL_IDX]:
+            var_types[ind] = kn.KN_VARTYPE_BINARY
+        for ind in data[s.INT_IDX]:
+            var_types[ind] = kn.KN_VARTYPE_INTEGER
+        kn.KN_set_var_types(kc, xTypes=var_types)
+
+        n_eq, n_ineq = A.shape[0], F.shape[0]
+
+        # Add the constraints to the problem.
+        if n_eq + n_ineq > 0:
+            kn.KN_add_cons(kc, n_eq + n_ineq)
+
+        # Add the equality bounds.
+        if n_eq > 0:
+            co_idxs = [i for i in range(n_eq)]
+            kn.KN_set_con_eqbnds(kc, indexCons=co_idxs, cEqBnds=b)
+
+        # Add the inequality bounds.
+        if n_ineq > 0:
+            co_idxs = [i for i in range(n_eq, n_eq + n_ineq)]
+            kn.KN_set_con_upbnds(kc, indexCons=co_idxs, cUpBnds=g)
+
+        # Set the constraint coefficients.
+        if n_eq + n_ineq > 0:
+            co_idxs, var_idxs, coefs = kn_coo(A, F)
+            kn.KN_add_con_linear_struct(kc, co_idxs, var_idxs, coefs)
+
+        # Set the objective function.
+
+        var_idxs = [i for i in range(n_vars)]
+        # Set the linear part of the objective function.
+        if q is not None:
+            kn.KN_add_obj_linear_struct(kc, var_idxs, q)
+
+        # Set the quadratic part of the objective function.
+        if P is not None:
+            P = P.tocoo()
+            var1_idxs, var2_idxs, coefs = kn_coo(P)
+            kn.KN_add_obj_quadratic_struct(kc, var1_idxs, var2_idxs, coefs)
+
+        # Set the sense of the objective function.
+        kn.KN_set_obj_goal(kc, kn.KN_OBJGOAL_MINIMIZE)
+
+        # Optimize the problem.
+        results = {}
+        try:
+            kn.KN_solve(kc)
+        except Exception:  # Error in the solution
+            results[s.STATUS] = s.SOLVER_ERROR
+
+        results[KNITRO.KEY] = kc
+
+        # Cache the Knitro context.
+        if solver_cache is not None:
+            solver_cache[self.name()] = kc
+
+        return results
+
+    def cite(self, data):
+        """Returns bibtex citation for the solver.
+
+        Parameters
+        ----------
+        data : dict
+            Data generated via an apply call.
+        """
+        return CITATION_DICT["KNITRO"]
